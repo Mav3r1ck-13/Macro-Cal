e38e06c7ee26bb48b5bab79efddc4e22f984bc73
Mav3r1ck-13 <angieyou3@gmail.com>
2023-01-10 09:48:52 -0500

    issues with git

---
+++ javascript/macrocalc.js
@@ -0,0 +1,27 @@
+//user input for protein and copying to protein goal
+
+function fillProteinGoal(protein) {
+    document.getElementById("proteinGoal").value = protein.value; 
+}
+
+//user input for carbs and copying to carbs goal
+
+function fillCarbsGoal(carbs) {
+    document.getElementById("carbGoal").value = carbs.value;
+}
+
+//user input for fats and copying to fats goal
+function fillFatsGoal(fats) {
+    document.getElementById("fatsGoal").value = fats.value
+}
+
+//calculate total calories based on user input
+function totalCal() {
+    let p = document.getElementById("proteinInput").value;
+    let c = document.getElementById("carbsInput").value;
+    let f = document.getElementById("fatsInput").value;
+    let calories = p * 4 + c * 4 + f * 9;
+    document.getElementById('output').value = parseInt(calories);
+}
+totalCal()
+
